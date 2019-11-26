e88d44072862c9a763551f0ab2fc936f5206eb01
tomo <tomotone77@gmail.com>
2019-11-26 14:56:54 +0000

    top page

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -1,6 +1,6 @@
-Rails.application.cofig.generators do |g|
-    g.stylesheets false
-    g.javascripts false
-    g.helper false
-    g.skip_routes true
+Rails.application.config.generators do |g|
+  g.stylesheets false
+  g.javascripts false
+  g.helper false
+  g.skip_routes true
 end
